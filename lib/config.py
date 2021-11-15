b1e6fc00f5353dee299f8155e95ced68df7fc9e1
Robert DelPrete <rdelprete@twilio.com>
2021-11-14 22:34:18 -0700

    first pass at adding server

--- lib/config.py
+++ lib/config.py
@@ -1,14 +1,18 @@
 import os
 import yaml
 
+
 def get_config():
     '''
     helper func to parse configfile
     '''
 
-    #TODO add some safety here
+    # TODO add some safety here
     trails_dir = os.path.dirname(os.path.realpath(__file__))
-    
+
+    if os.environ.get("TRAILS_CONFIG"):
+        trails_dir = os.environ.get("TRAILS_CONFIG")
+
     with open(trails_dir + "/../config.yaml", "r") as f:
-      config = yaml.load(f.read(), Loader=yaml.SafeLoader)
+        config = yaml.load(f.read(), Loader=yaml.SafeLoader)
     return config

0cecd647f8ba4a48f60673bab8dcb4e9481a1ab2
Robert DelPrete <rdelprete@twilio.com>
2021-11-14 22:37:31 -0700

    fixed bug

--- lib/config.py
+++ lib/config.py
@@ -8,11 +8,12 @@ def get_config():
     '''
 
     # TODO add some safety here
-    trails_dir = os.path.dirname(os.path.realpath(__file__))
+    trails_dir = os.path.dirname(
+        os.path.realpath(__file__)) + "/../config.yaml"
 
     if os.environ.get("TRAILS_CONFIG"):
         trails_dir = os.environ.get("TRAILS_CONFIG")
 
-    with open(trails_dir + "/../config.yaml", "r") as f:
+    with open(trails_dir, "r") as f:
         config = yaml.load(f.read(), Loader=yaml.SafeLoader)
     return config

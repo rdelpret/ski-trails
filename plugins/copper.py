9b51fe43b3aa876cf7c6439f92b8cbf92d91a4fa
jamiedel818 <jamiedel818@gmail.com>
2021-11-18 13:25:07 -0500

    initial plugin commit

---
+++ plugins/copper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+import json
+import yaml
+
+'''
+Data Structure
+trails = {"resort" : "", "areas" : []}
+area   = {"name" : "", "trails" : []}
+trail  = {"name" : "", "status" : "", "rating" : ""}
+'''

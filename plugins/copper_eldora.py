b7194a7bafd07facd5a6e936d63ad35714a1f134
Duncan Finney <duncan.finney@gmail.com>
2023-10-23 04:51:33 -0600

    fix: Upgrading for 2023-2024 season

--- plugins/copper_eldora.py
+++ plugins/copper_eldora.py
@@ -13,7 +13,8 @@ difficulty_map =  {
   'easier': 'green',
   'more_difficult': 'blue',
   'most_difficult': 'black',
-  'extremely_difficult': 'double'
+  'extremely_difficult': 'double',
+  'extremely_difficult_2': 'double',
 }
 
 def cleanup_status(status):
@@ -25,10 +26,16 @@ def cleanup_status(status):
 
 def copper_eldora(resort):
     url = "https://%s/api/v1/dor/status" % resort["urlBase"]
-    data = requests.get(url).json()
-    data = list(filter(lambda d: d["subtype"] == "alpine trail", data))
-
+    data_str = requests.get(url)
     trails = {"name" : resort["name"], "areas" : []}
+    try: 
+        data = data_str.json()
+    except:
+        print("Error parsing json from %s" % url)
+        print(f"response: {data_str}")
+        return trails    
+
+    data = list(filter(lambda d: d["subtype"] == "alpine trail", data))
 
     # pull the areas out of the data
     area_lookup = {}
@@ -41,6 +48,10 @@ def copper_eldora(resort):
         trails["areas"].append(area)
 
     for tr in data:
+        subtype = tr["properties"]["subtype"]
+        if tr["properties"]["subtype"] not in difficulty_map:
+            print("Unknown difficulty for trail: " + tr["properties"]["subtype"] + " | " + resort["name"]  + " | " + tr["properties"]["name"])
+            continue
         trail = {
             "name": tr["properties"]["name"],
             "rating": difficulty_map[tr["properties"]["subtype"]],
